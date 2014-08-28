dfb4931ee0f0367652912b6d43055df58bcd345d
Lorenzo Fermi <lorenzofermi@gmail.com>
2014-08-28 23:56:19 +0200

    Initial Commit

---
+++ iHelloWorld/iHWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iHWAppDelegate.h
+//  iHelloWorld
+//
+//  Created by Lorentz on 28/08/2014.
+//  Copyright (c) 2014 lorentz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iHWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iHelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iHelloWorld
+//
+//  Created by Lorentz on 28/08/2014.
+//  Copyright (c) 2014 lorentz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iHWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([iHWAppDelegate class]));
+   }
+}
